88018e26780abfd15db800979fe7b6e110d6be91
LusyWolf <lucy.wolk@ya.ru>
2016-11-06 00:35:26 +0300

    НОК
    Вычисляет наименьшее общее кратное с помощью алгоритма Евклида.

---
+++ nok.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+int main (){
+int a, b, c, d;
+cin>>a>>b;
+d=a*b;
+if(a==b)
+{
+c=a;
+}
+else {
+while ((a!=0)&&(b!=0)){
+if(a>b){
+a=a%b;}
+else if(b>a)
+{b=b%a;}
+
+}
+c=a+b;
+}
+cout<<d/c;
+}
